140fdc5d46c404e7e50371c9e288dd9db87fe017
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-04 21:21:14 +0200

    Update user.c

--- src/user.c
+++ src/user.c
@@ -4,6 +4,28 @@
 #include <string.h>
 #include <time.h>
 
+void generate_token(char* token) {
+    const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int num_characters = sizeof(characters) - 1;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < TOKEN_LENGTH; i++) {
+        int index = rand() % num_characters;
+        token[i] = characters[index];
+    }
+
+    token[TOKEN_LENGTH] = '\0';
+}
+
+User* create_user(const char* username) {
+    User* user = (User*)malloc(sizeof(User));
+    strncpy(user->username, username, MAX_USERNAME_LENGTH);
+    generate_token(user->token);
+    generate_public_address(user);
+    return user;
+}
+
 void generate_public_address(User* user) {
     // Générez une adresse publique unique ici
     // Vous pouvez utiliser une logique basée sur le pseudonyme, le token, etc.
@@ -11,5 +33,3 @@ void generate_public_address(User* user) {
     // Exemple simple : concaténez le pseudonyme et le token
     snprintf(user->public_address, sizeof(user->public_address), "%s_%s", user->username, user->token);
 }
-
-// Reste du code...
