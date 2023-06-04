fca1f4369522fb4177d2a1b56a92f71e50856338
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-04 21:19:44 +0200

    Update user.h

--- src/user.h
+++ src/user.h
@@ -7,8 +7,10 @@
 typedef struct {
     char username[MAX_USERNAME_LENGTH];
     char token[TOKEN_LENGTH + 1];
+    char public_address[50];
 } User;
 
 User* create_user(const char* username);
+void generate_public_address(User* user);
 
 #endif
