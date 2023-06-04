b9beaea898306b84d104b39888bc15e49bf007a9
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-04 21:21:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,30 +2,23 @@
 #include <stdlib.h>
 #include "user.h"
 
-void registration() {
-    char username[MAX_USERNAME_LENGTH];
-    printf("Enter a username: ");
-    scanf("%s", username);
-
-    User* user = create_user(username);
-    printf("Registration successful!\n");
+void display_profile(User* user) {
     printf("Username: %s\n", user->username);
     printf("Token: %s\n", user->token);
-
-    // Vous pouvez enregistrer l'utilisateur dans la base de données à ce stade
-
-    free(user);
+    printf("Public Address: %s\n", user->public_address);
 }
 
 int main() {
-    // Autres fonctionnalités de votre application
-    // ...
+    // Création d'un utilisateur
+    User* user = create_user("John");
 
-    registration();
+    // Affichage du profil de l'utilisateur
+    display_profile(user);
 
-    // Autres fonctionnalités de votre application
-    // ...
+    // Autres fonctionnalités à ajouter selon votre cahier des charges
+
+    // Libération de la mémoire allouée pour l'utilisateur
+    free(user);
 
     return 0;
 }
-
