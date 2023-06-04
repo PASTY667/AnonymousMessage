1adaccb7db33df38706ce1cb607d7b85d05c0f29
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-04 21:15:19 +0200

    Update user.h

---
+++ src/user.h
@@ -0,0 +1,12 @@
+#ifndef USER_H
+#define USER_H
+
+#define MAX_USERNAME_LENGTH 50
+#define TOKEN_LENGTH 10
+
+typedef struct {
+    char username[MAX_USERNAME_LENGTH];
+    char token[TOKEN_LENGTH + 1];
+} User;
+
+#endif
